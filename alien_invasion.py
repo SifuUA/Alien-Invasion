a11671c6cc88277f2b0fd56c9878ab48a281192a
SifuUA <alekseykres@gmail.com>
2020-08-17 23:26:23 +0300

    Added quit button and some refactoring

--- alien_invasion.py
+++ alien_invasion.py
@@ -32,15 +32,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.ship.moving_left = True
+                self.check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.ship.moving_right = False
-                if event.key == pygame.K_LEFT:
-                    self.ship.moving_left = False
+                self.check_keyup_events(event)
+
+    def check_keyup_events(self, event):
+        """Respond to key press"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def check_keydown_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen"""
