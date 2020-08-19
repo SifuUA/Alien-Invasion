85dbf478c1e1fdb26e90e88691b8c8344a1618e0
SifuUA <alekseykres@gmail.com>
2020-08-19 23:48:18 +0300

    Present alien on screen

--- alien.py
+++ alien.py
@@ -5,19 +5,18 @@ from pygame.sprite import Sprite
 class Alien(Sprite):
     """A class to represent a single alien in the fleet"""
 
+    def __init__(self, ai_game):
+        """Initialize the alien and set its starting position"""
+        super().__init__()
+        self.screen = ai_game.screen
 
-def __init__(self, ai_game):
-    """Initialize the alien and set its starting position"""
-    super().__init__()
-    self.screen = ai_game.screen
+        # Load the alien image and set its rect attribute
+        self.image = pygame.image.load('images/alien.bmp')
+        self.rect = self.image.get_rect()
 
-    # Load the alien image and set its rect attribute
-    self.image = pygame.image.load('images/alien.bmp')
-    self.rect = self.image.get_rect()
+        # Start each new alien near the top of the screen
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
 
-    # Start each new alien near the top of the screen
-    self.rect.x = self.rect.width
-    self.rect.y = self.rect.height
-
-    # Store the alien`s exact horizontal position
-    self.x = float(self.rect.x)
+        # Store the alien`s exact horizontal position
+        self.x = float(self.rect.x)
